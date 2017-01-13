eeefd68a0e998739743e554422c71e1f1b986b1d
Cheng Ge <chengge@MBP.local>
2017-01-12 19:24:57 -0500

    implement require_is_admin? before admin panel

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,5 +1,7 @@
 class Admin::JobsController < ApplicationController
     before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
+    before_action :require_is_admin
+
     def index
         @jobs = Job.all
     end
@@ -44,5 +46,12 @@ class Admin::JobsController < ApplicationController
 
     def job_params
         params.require(:job).permit(:title, :description)
+    end
+
+    def require_is_admin
+        if !current_user.admin?
+            flash[:alert] = 'You are not admin'
+            redirect_to root_path
+        end
     end
 end
