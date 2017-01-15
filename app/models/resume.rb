4f426abd10e99260ebf2fa5af989ed29d60c4f90
Cheng Ge <chengge@MBP.local>
2017-01-15 16:01:55 -0500

    implement uploading resume pdf

--- app/models/resume.rb
+++ app/models/resume.rb
@@ -1,6 +1,8 @@
 class Resume < ApplicationRecord
-    validates :content, presence: true
+  belongs_to :user
+  belongs_to :job
 
-    belongs_to :user
-    belongs_to :job
+  mount_uploader :attachment, AttachmentUploader
+
+  validates :content, presence: true
 end
